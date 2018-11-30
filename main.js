9206da2e87523d6fc5c3c73723baf4aeaa6f73ba
Roy <linq@keba.com>
2018-11-30 17:01:57 +0800

    change the file locate
    Signed-off-by: Roy <linq@keba.com>

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var scene;
 var man;
 var timer;
 var monster;
+var arrow;
+var shoottimer;
 
 function Scene() {
   this.width = 1024;
@@ -26,7 +28,7 @@ function man() {
   this.width = 20;
   this.height = 20;
   this.direct = "absolute";
-  this.imgsrc = "man.jpg";
+  this.imgsrc = "resoucre/man.jpg";
   this.position = "absolute";
   this.body = [1, 1, "black", null];
   this.setDirect = function(code) {
@@ -71,6 +73,26 @@ function man() {
   };
 }
 
+function Arrow() {
+  this.width = 20;
+  this.height = 20;
+  this.imgsrc = "resoucre/food.jpg";
+  this.position = "absolute";
+  this.body = [scene.width - man.body[1] * this.height * 25, 500, null];
+  this.show = function() {
+    if (this.body[2] == null) {
+      this.body[2] = document.createElement("img");
+      this.body[2].src = this.imgsrc;
+      this.body[2].style.position = this.position;
+      scene._map.appendChild(this.body[2]);
+    }
+    this.body[2].style.left =this.body[0] + "px";
+    this.body[2].style.top =this.body[1]+ "px";
+    this.body[1] = this.body[1] -10; 
+  };
+  this.show();
+}
+
 function Monster() {
   this.width = 40;
   this.height = 40;
@@ -83,7 +105,7 @@ function Monster() {
   this.show = function() {
     if (this._aim == null) {
       this._aim = document.createElement("img");
-      this._aim.src = "smile.jpg";
+      this._aim.src = "resoucre/smile.jpg";
       this._aim.style.position = this.position;
 
       scene._map.appendChild(this._aim);
@@ -114,10 +136,13 @@ window.onload = function() {
     } else {
       code = event.keyCode;
     }
-    if (code === 38) {
-      //shoot
-    } else {
-      man.setDirect(code);
+    if (code ===38){
+        arrow = new Arrow();
+        arrow.show();
+        shoottimer =setInterval(arrow.show(), 50);
+    }else{
+        man.setDirect(code);
     }
+
   };
 };
